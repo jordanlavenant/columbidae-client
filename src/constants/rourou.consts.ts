7d6e2c904311d40edeee1fc18a42fb36d9457a00
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2026-02-11 23:58:51 +0100

    tech: adding rourou_types const

---
+++ src/constants/rourou.consts.ts
@@ -0,0 +1,9 @@
+export enum ROUROU_TYPES {
+  CASUAL_ROUROU = 'casual_rourou',
+  LAUGHING_ROUROU = 'laughing_rourou',
+  DOUBTFUL_ROUROU = 'doubtful_rourou',
+  ADMIRING_ROUROU = 'admiring_rourou',
+  ANGRY_ROUROU = 'angry_rourou',
+  SURPRISED_ROUROU = 'surprised_rourou',
+  SAD_ROUROU = 'sad_rourou',
+}
