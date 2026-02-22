ea355e9a40bc5b5df4d81f0186027d6386283a37
Marc Roussel <marc.roussel04@outlook.fr>
2026-02-22 13:43:43 +0100

    feat: disabling header when user is not logged in

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,46 @@
 import PostForm from '@/components/PostForm/PostForm'
 import { Button } from '@/components/ui/button'
 import { useAuth } from '@/hooks/use-auth'
+import { LogOutIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
-  const { logout } = useAuth()
+  const { currentUser, logout } = useAuth()
+  const [isUserConnected, setIsUserConnected] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (currentUser != undefined) {
+      setIsUserConnected(true)
+    } else {
+      setIsUserConnected(false)
+    }
+  }, [currentUser])
 
   return (
-    <header className="sticky top-0 z-50 border-b bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-14 items-center justify-between px-4">
-        <img
-          src="./columbidae_logo_app.svg"
-          alt="Columbidae Logo"
-          className="h-[3em]"
-        />
-        <h1 className="font-[Alan_Sans] font-extrabold text-3xl">Columbidae</h1>
+    isUserConnected && (
+      <header className="sticky top-0 z-50 border-b bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container mx-auto flex h-14 items-center justify-between px-4">
+          <div className="flex gap-2 items-center">
+            <img
+              src="./columbidae_logo_app.svg"
+              alt="Columbidae Logo"
+              className="h-[3em]"
+            />
+            <h1 className="font-[Alan_Sans] font-extrabold text-3xl">
+              Columbidae
+            </h1>
+          </div>
 
-        <PostForm />
-        <Button variant="destructive" onClick={logout}>
-          Disconnect
-        </Button>
-      </div>
-    </header>
+          <div className="flex gap-2 items-center">
+            <PostForm />
+            <Button variant="destructive" onClick={logout}>
+              <LogOutIcon />
+              Se déconnecter
+            </Button>
+          </div>
+        </div>
+      </header>
+    )
   )
 }
 
